30f1ebcf489d6207cf3c32b51cd8bbb4ba75c709
Iztok Bajcar <ib8548@student.uni-lj.si>
2024-10-12 01:01:32 +0200

    Renamed the playlist attribute to m3u8

--- src/bandcamp_link_scraper/api.py
+++ src/bandcamp_link_scraper/api.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/songs/")
 async def root(url: str):
     playlist = get_songs(url)
-    return {"playlist": playlist}
+    return {"m3u8": playlist}
 
 if __name__ == "__main__":
     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
